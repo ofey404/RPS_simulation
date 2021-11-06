f3dd556f91fd043e65e77cc1fe77c87253d099d6
Ofey Chan <ofey206@gmail.com>
2021-11-06 17:09:29 +0800

    Add readme

---
+++ sketch/README.md
@@ -0,0 +1,30 @@
+简单的 RPS 模拟工作流程
+===================
+
+运行 `./new.sh` 就可以迅速交互式进行实验。
+
+```bash
+# NOTICE: Change to what you like!
+EDITOR=vim
+FILE_MANAGER=xdg-open
+```
+
+运行 `./new.sh` 将会在 `data` 目录下新建一个目录，并打开其中的 `config.json` 进行编辑。编辑完之后执行模拟和绘图的命令，执行完之后会自动打开目录用于预览。
+
+-----
+
+目录下文件的简要介绍：
+
+```bash
+.
+├── data/       # 数据目录
+├── template/   # 示例配置文件
+├── ... 
+├── new.sh      # 入口脚本
+├── main.py     # 计算脚本
+├── plot.py     # 绘图脚本
+├── ...
+├── util.py     # 公用函数
+└── README.md   # 本文件
+```
+
--- sketch/new.sh
+++ sketch/new.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
+# NOTICE: Change to what you like!
+EDITOR=vim
+FILE_MANAGER=xdg-open
+
 date_str=$(date +%Y-%m-%d-%H:%M:%S)
 
 mkdir -p $SCRIPT_DIR/data/$date_str
 cp $SCRIPT_DIR/template/config.json $SCRIPT_DIR/data/$date_str/config.json
 
-vim $SCRIPT_DIR/data/$date_str/config.json
+$EDITOR $SCRIPT_DIR/data/$date_str/config.json
 
 python $SCRIPT_DIR/main.py $SCRIPT_DIR/data/$date_str/config.json
 python $SCRIPT_DIR/plot.py $SCRIPT_DIR/data/$date_str/config.json
 
-xdg-open $SCRIPT_DIR/data/$date_str/
+$FILE_MANAGER $SCRIPT_DIR/data/$date_str/
 
