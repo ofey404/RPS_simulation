func: add correct average calculation

sketch/plot.py
@@ -71,6 +71,51 @@ class Visualizer:
         plt.savefig(filepath)
         plt.close()
 
+    def plt_temporal_mass_average(self, filepath):
+        result = self.data["result"]
+
+        x = np.arange(self.data["shape"][0])
+        avg = np.average(result, axis=1)
+        var = np.var(result, axis=1)
+
+        log_avg = np.log(avg)
+        k, b  = np.polyfit(x, log_avg, 1)
+
+        plt.scatter(x, log_avg)
+        plt.plot(x, k * x + b, "k-")
+        # plt.show()
+        plt.savefig(filepath)
+        plt.close()
+
+    def plt_temporal_mass_average_visualization(self, filepath):
+        fig, ax = plt.subplots(subplot_kw={"aspect": "equal"})
+        result = self.data["result"]
+        x = np.arange(self.data["shape"][0])
+        avg = np.average(result, axis=1)
+
+        n = x.shape[0]
+        previous_xy = None
+        for i in range(n):
+            xy = (x[i], 0 if i % 2 == 0 else 1)
+            diameter = avg[i]
+            ax.add_artist(Circle(xy=xy, diameter=diameter, color=self.color_scale[i]))
+            # ax.add_artist(Circle(xy=xy, diameter=diameter))
+            if previous_xy:
+                ax.arrow(
+                    previous_xy[0],
+                    previous_xy[1],
+                    xy[0] - previous_xy[0],
+                    xy[1] - previous_xy[1],
+                )
+            previous_xy = xy
+
+        ax.set_xlim(-self.border, n + self.border)
+        ax.set_ylim(-self.border, self.border)
+
+        # plt.show()
+        plt.savefig(filepath)
+        plt.close()
+
 
 def main(argv):
     config, data_dir = parse_argv(argv)
@@ -83,11 +128,12 @@ def main(argv):
         data = pickle.load(data_file)
         n, t = data["shape"]
         v = Visualizer(data, config)
-        for plot_ts in range(0, t, step):
-            v.plt_log_x(plot_ts, data_dir / "log_plt_{}.png".format(plot_ts))
-            v.plt_visualize_1D(
-                plot_ts, data_dir / "visualization_{}.png".format(plot_ts)
-            )
+        v.plt_temporal_mass_average(data_dir / "temporal_mass_average.png")
+        v.plt_temporal_mass_average_visualization(data_dir / "temporal_mass_average_visualization.png")
+        exit(0)
+        for i in range(0, t, step):
+            v.plt_log_x(i, data_dir / "log_plt_{}.png".format(i))
+            v.plt_visualize_1D(i, data_dir / "visualization_{}.png".format(i))
 
 
 if __name__ == "__main__":